5cb3719abb3fadcc31cc53ddcb30cb59d9c75ee0
David North <david.north@oc.edu>
2019-01-31 16:36:24 -0600

    Add files via upload
    Initial Load

---
+++ routes/students.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var router = express.Router();
+
+function validate(student) {
+	var errorMessage="[";
+	if (student.idNumber.length == 0) {
+		errorMessage+= '{"attributeName":"idNumber" , "message":"Must have idNumber"}';		
+	}
+	if (student.firstName.length == 0){
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"firstName", "message":"Must have first name"}';
+	}
+	if (student.lastName.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"lastName" , "message":"Must have last name"}';		
+	}
+	if (student.zip.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"zip" , "message":"Must have zip code"}';		
+	}
+	if (student.city.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"city" , "message":"Must have city"}';		
+	}
+	if (student.state.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"state" , "message":"Must have state"}';		
+	}
+	if (student.email.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"email" , "message":"Must have email"}';		
+	}
+	if (student.classification.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"classification" , "message":"Must have classification"}';		
+	}
+	if (student.gender.length == 0) {
+		if(errorMessage.length>1) errorMessage+=",";
+		errorMessage+= '{"attributeName":"gender" , "message":"Must have gender"}';		
+	}
+	errorMessage +="]";
+	return errorMessage
+}
+
+/* GET student listing. */
+router.get('/', function(req, res, next) {
+	if (req.query.page == null) 
+		var offset = 0;
+	else
+		var offset = parseInt(req.query.page);
+	if (req.query.per_page==null)
+		var limit = 20;
+	else
+		var limit = parseInt(req.query.per_page);
+	res.locals.connection.query('SELECT * from student limit ? offset ?', [limit, offset],function (error, results, fields) {
+	  	if(error){
+			res.status(500);
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+			res.status(200);
+  			res.send(JSON.stringify(results));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+router.get('/:id', function(req, res, next) {
+	var id = req.params.id;
+	res.locals.connection.query('SELECT * from student where id=?',id, function (error, results, fields) {
+	  	if(error){
+			res.status(500);
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+			res.status(200);
+  			res.send(JSON.stringify(results));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+router.put('/:id', function(req, res, next) {
+	var id = req.params.id;
+	console.log(req.body);
+	
+	var student = req.body;
+	let errorMessage = validate(student);
+	if(errorMessage.length >2 ){
+		res.status(406);
+		res.send(errorMessage); 
+	}
+	else {
+	res.locals.connection.query('Update student set ? where id=?',[req.body, id], function (error, results) {
+	  	if(error){
+			res.status(500);
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+			res.status(200);
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+	  });
+	}
+});
+router.post('/', function(req, res, next) {
+	var id = req.params.id;
+	console.log(req.body);
+	
+	var student = req.body;
+	let errorMessage = validate(student);
+	if(errorMessage.length >2 ){
+		res.status(406);
+		res.send(errorMessage); 
+	}
+	else {
+		res.locals.connection.query('Insert into student set ? ',req.body, function (error, results) {
+			if(error){
+				res.status(500);
+				res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+				//If there is error, we send the error in the error section with 500 status
+			} else {
+				res.status(200);
+				res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+				//If there is no error, all is good and response is 200OK.
+			}
+		});
+	}
+});
+
+router.delete('/:id', function(req, res, next) {
+	var id = req.params.id;
+	res.locals.connection.query('Delete from student where id=?',id, function (error, results) {
+	  	if(error){
+			res.status=500;
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+			res.status=200;
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+module.exports = router;
